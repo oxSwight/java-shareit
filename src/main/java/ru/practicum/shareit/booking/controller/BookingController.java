9cdf805dd67408fb26b452ca42259573f2cea882
IgorZ <igorzomm3r@gmail.com>
2025-05-14 18:35:38 +0200

    ТЗ 14

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,11 +1,10 @@
-package ru.practicum.shareit.booking;
+package ru.practicum.shareit.booking.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * TODO Sprint add-bookings.
- */
+@Slf4j
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingController {
