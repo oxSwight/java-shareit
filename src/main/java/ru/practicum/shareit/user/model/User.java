959a1cab71f94bf24d21f947705c874eabe8df19
IgorZ <igorzomm3r@gmail.com>
2025-05-30 22:15:16 +0200

    импорт удалил ненужный

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 import lombok.*;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
